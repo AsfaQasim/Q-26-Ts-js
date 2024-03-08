eaac243eff756505dec8ef3a0997a1d39627db8b
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-08 08:38:19 -0800

    Q 26 of Ts/js & node js of 45 assignment.
    Q 26 of Ts/js & node js of 45 assignment.

---
+++ main.js
@@ -0,0 +1,15 @@
+//choose a color for an alien_color
+//did in Exercise 25, and write an if-else chain.
+var alien_color = "green";
+//If the aliens color is green, print a statement that
+//the player just earned 5 points for shooting the alien.
+if (alien_color == "green") {
+    console.log("the player just earned 5 points for shooting te alien.");
+}
+else {
+    console.log("the player just earned 10 points.");
+}
+alien_color = "red";
+if (alien_color == "green") {
+    console.log("the player just earned 5 points for shooting the alien.");
+}
